80b56ad52be9b12a57eabb01eb218f4e5ed11186
Query <65344885+QueryOne@users.noreply.github.com>
2020-06-07 00:05:36 +1000

    Update and rename setup.js to live-log-nexus.js

--- Live-Logs/live-log-nexus.js
+++ Live-Logs/live-log-nexus.js
@@ -1,4 +1,6 @@
-
+LL = (function() {
+  
+})()
 
 var packageID;
 
@@ -8,64 +10,6 @@ if (n == 0) {
 }
 packageID = n;
 
-ng = (function() {
-  var random = function(min,max) { return Math.floor(Math.random() * (max - min + 1) + min) }
-  var grandom = function(list) { return list[random(0, list.length - 1)] }
-  
-  var firstName = [
-     'adorable','agreeable','amazing','ancient','artistic',
-     'belligerent','brainy',
-     'capricious','chemical','competitive','confused',
-     'dainty','dangerous','daring','delightful',
-     'earthy','envious',
-     'fanciful','flatulent',
-     'genius','glamorous','gleeful','groovy',
-     'handsome','heady','hilarious','hypnotic',
-     'incredible','inexpensive','inquisitive','intelligent',
-     'jazzy','juicy',
-     'keen','kind',
-     'lamentable','lonely','luxuriant',
-     'macabre','macho','majestic','marvelous','musical','mysterious',
-     'necessary','needy','nonstop',
-     'opulent','overconfident',
-     'pastoral','penitent','purposeful',
-     'quaint',
-     'reckless','rich',
-     'scientific','sonorous','soporific',
-     'tantalising','tepid','treacherous',
-     'uptight',
-     'vain',
-     'winsome',
-  ]
-  var lastName = [
-     'antelope','armadillo','axolotl',
-     'banana','bat','bee',
-     'capybara','cat','cuttlefish',
-     'dodo','dolphin','dotterel',
-     'echidna','elephant',
-     'fox',
-     'giraffe','gnu','goldfish','gorilla','grouse',
-     'hamster','herring','hyena',
-     'ibex','impala',
-     'jaguar', 
-     'kouprey',
-     'lemur','llama',
-     'meerkat',
-     'narwhal',
-     'ocelot','osprey',
-     'penguin','pika','porpoise',
-     'quokka',
-     'raccoon','raven','reindeer',
-     'scorpion','serval','shark','sheep','stallion',
-     'toucan',
-     'walrus','weasel','wombat',
-     'zebra',
-  ]
-  
-  return {
-    name: function() { return grandom(firstName) + '-' + grandom(lastName) },   
-  }
-})()
 
 LE = (function() {
   var _salt = 'live-logs-salt-encrypted'
