379c64c563c23dcd6f92fcaa4f34db364ae8560d
Query <65344885+QueryOne@users.noreply.github.com>
2020-06-09 18:10:51 +1000

    Update live-logs-interface.js

--- Live-Logs/live-logs-interface.js
+++ Live-Logs/live-logs-interface.js
@@ -1,4 +1,18 @@
 (function() {
+  var script = `
+console.log(args);
+if (args[1] == 'undefined') {
+  if (LL.checkToken()) {
+    LL.upload()
+  } else {
+    LL.inputToken()
+  }
+} else {
+  var t = args[1].split(' ');
+  console.log(t);
+}
+`
+  
   var nexusInterface = 'Live-logs';
   /* This really shouldn't happen, as you invoke the download from "Live-logs" */
   if (client.package_exists(nexusInterface) == 0) { client.package_create(nexusInterface) }
@@ -6,9 +20,9 @@
   var package = client.get_package(nexusInterface);
   var init = client.reflex_create(package, 'init', 'alias');
       init.text = '-L([ ]+(.*))*$';
-      init.matching = 'regex';
+      init.matching = 'regexp';
       init.actions = [
-      
+        {action:'script', script:script},
       ];
       
   client.gmcp_save_system(true);  
