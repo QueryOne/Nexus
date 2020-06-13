1081dc538eaf4f15ff2755246f6263dc8f4098c6
Query <65344885+QueryOne@users.noreply.github.com>
2020-06-13 19:43:15 +1000

    Update pm.main.js

--- PackageManager/pm.main.js
+++ PackageManager/pm.main.js
@@ -13,9 +13,11 @@ pm = (function() {
       // Dimensions, Positioning
       cssRules += '#pm-main {position:absolute; right:11px; top:-258px; height:250px; width:360px;}\n'
       cssRules += '#pm-closer {position:absolute; right:4px; top:4px; height:17px; width:17px;}\n'
+      cssRules += '#pm-header {position:absolute; left:0%; top:0%; height:18px; width:calc(100%-17px-4px);}\n'
       // Styling
       cssRules += '#pm-main {border:1px solid rgba(88,14,14,0.33); border-radius:3px; background:rgba(22,19,17,1); background:"raster.png";}\n'
       cssRules += '#pm-closer {cursor:pointer;}\n'
+      cssRules += '#pm-header {border-bottom:1px solid rgba(88,14,14,0.33);}\n'
       // Font
       cssRules += '#pm-closer {color:rgba(200,22,22,1);}\n'
       cssRules += '#pm-closer:hover {color:rgba(255,22,22,1); text-align:center;}\n'
