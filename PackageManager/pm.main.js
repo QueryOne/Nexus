61ae232c7a86efe7efd5c575892ce417b68477e9
Query <65344885+QueryOne@users.noreply.github.com>
2020-06-13 12:52:13 +1000

    Update pm.main.js

--- PackageManager/pm.main.js
+++ PackageManager/pm.main.js
@@ -1,17 +1,27 @@
 pm = (function() {
+  var prefix = '<span color="rgba(44,44,44,1)">(<span color="rgba(155,177,177,1)">pm</span>)</span>'
+  var defaultColor = 'rgba( 231, 144, 33, 1 )'
+  
+  var report = function(msg) { ow_Write('#main', prefix + '<span color="' + defaultColor + '">' + msg + '</span>') }
+  
   var defaults = [
     
   ]
   var packages = []
 
   var initialise = function() {
+    report('Initialising Package Manager.')
     // Locate saved variables
+    report('- Locating saved variables.')
     
     // Evaluate list of packages
+    report('- Evaluating package list.')
     
     // Download approved packages
+    report('- Downloading approved packages.')
     
     // Provide interface
+    report('- Beginning interface.')
     
     // Provide initialised prompt
     
@@ -20,5 +30,6 @@ pm = (function() {
   return {
     initialise: initialise,
     packages  : packages,
+    report    : report,
   }
 })()
