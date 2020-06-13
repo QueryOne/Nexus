69c9247c9a7d1589d7e407d9aafeedb4f094d659
Query <65344885+QueryOne@users.noreply.github.com>
2020-06-13 13:37:29 +1000

    Update pm.main.js

--- PackageManager/pm.main.js
+++ PackageManager/pm.main.js
@@ -1,18 +1,59 @@
 pm = (function() {
-  var prefix = '<span style="color:rgba(44,44,44,1);">(<span style="color:rgba(155,177,177,1);">pm</span>)</span>'
-  var defaultColor = 'rgba( 231, 144, 33, 1 )'
+  // Options
+  var prefix = '<span style="color:rgba(44,44,44,1);">(<span style="color:rgba(111,177,177,1);">pm</span>)</span>'
+  var defaultColor = 'rgba( 177, 144, 53, 1 )'
+  var pmKey = 'pmPackages'
+  var pmOutput = 'output_main'
+  var pmOutput2 = 'pm-main'
   
-  var report = function(msg) { ow_Write('#output_main', prefix + '<span style="color:' + defaultColor + ';"> ' + msg + '</span>') }
+  // CSS
+  var cssClass = 'pm-css'
+  var cssRules = ''
+      // Dimensions
+      cssRules += '#pm-main {position:absolute; right:1.2em; top:-400px; height:400px; width:600px;}\n'
+      // Styling
+      cssRules += '#pm-main {border:1px solid rgba(88,14,14,1); border-radius:3px; background:rgba(56,14,14,1);'
+      // Font
+  
+  var css = function(rule) {
+    $('.' + cssClass).remove()
+    $('body').append('<div class="' + cssClass + '">&shy;<style>' + rule + '</style></div>')
+  }
+  
+  var swap = function() { [pmOutput, pmOutput2] = [pmOutput2, pmOutput] }
+  var report = function(msg) { ow_Write('#' + pmOutput, prefix + '<span style="color:' + defaultColor + ';"> ' + msg + '</span>') }
   
   var defaults = [
     
   ]
   var packages = []
-
+  
+  var draw = function() {
+    // 
+    // Anchor this to #input
+    var build  = ''
+        build += '<div id="' + pmOutput2.replace('#','') + '"'
+        build += '<div id="pm-closer">x</div>'
+        build += '' + '</div>'
+    $('#input').append(build)
+  }
+  
   var initialise = function() {
     report('Initialising Package Manager.')
+    // Exit the #main_output flow
+    css();
+    draw();
+    swap();
+    
+    
     // Locate saved variables
     report('- Locating saved variables.')
+    for (const [k, v] of Object.entries(client.vars)) {
+      if (k == pmKey) {
+        packages = v
+        break
+      }
+    }
     
     // Evaluate list of packages
     report('- Evaluating package list.')
