a08ff95c6ab4bf0d24e1a63c8124c1a31d3ce851
Query <65344885+QueryOne@users.noreply.github.com>
2020-06-13 12:42:10 +1000

    Create pm.main.js

---
+++ PackageManager/pm.main.js
@@ -0,0 +1,24 @@
+pm = (function() {
+  var defaults = [
+    
+  ]
+  var packages = []
+
+  var initialise = function() {
+    // Locate saved variables
+    
+    // Evaluate list of packages
+    
+    // Download approved packages
+    
+    // Provide interface
+    
+    // Provide initialised prompt
+    
+  }
+
+  return {
+    initialise: initialise,
+    packages  : packages,
+  }
+})()
